d1a8080bf5f3f6f3c11d2abd605e0bbebf539011
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-02-07 08:00:58 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  separator = "----------"
+  separator = "-----------"
   puts "   |   |   "
   puts separator
   puts "   |   |   "
